4a9f3d17e05d4a59e7662262df6cae20f2d0d80f
rafaeldamiam <41579736+rafaeldamiam@users.noreply.github.com>
2018-11-23 14:47:09 -0300

    att
    compra

--- bibliotecas/mysqli.php
+++ bibliotecas/mysqli.php
@@ -1,15 +1,11 @@
 <?php
-
 require_once ("./servicos/manipulacao.php");
 
 function conn() {
 	$dados = manipulacao();
-
     $cnx = mysqli_connect($dados[0], $dados[1], $dados[2], $dados[3]);
-
     if (!$cnx) die('Deu errado a conexão!');
+    mysqli_set_charset($cnx, "utf8"); 
 
-    mysqli_set_charset($cnx, "utf8");
-    
     return $cnx;
 }
--- visao/compra/formulario.visao.php
+++ visao/compra/formulario.visao.php
@@ -22,6 +22,19 @@
 
 
 
+	<form action="ex-enquete.php" method="POST">
+		<input type="radio" name="enquete" value="Tá tranquila, tá favorável">
+		<label for="enquete">Tá tranquila, tá favorável</label><br>
+		<input type="radio" name="enquete" value="Está complicadíssima">
+		<label for="enquete">Está complicadíssima</label><br>
+		<input type="radio" name="enquete" value="Já foi pior">
+		<label for="enquete">Já foi pior</label><br>
+		<input type="radio" name="enquete" value="Tende a piorar">
+		<label for="enquete">Tende a piorar</label><br><br>
+		<input type="submit" name="Vai enquete!">
+	</form>
+
+
 <br><br><br>
 <button id="botao" type="submit">Enviar</button>
 </form>
